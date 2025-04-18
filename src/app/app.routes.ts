08789b48b02e693b07c2d147930621172946a94f
kienluong01 <kiennlph50167@gmail.com>
2025-04-18 17:38:56 +0700

    base

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { ListComponent } from './list/list.component';
+import { CreateComponent } from './create/create.component';
+import { EditComponent } from './edit/edit.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+      {
+            path:'list',
+            component:ListComponent
+      },
+      {
+            path:'create',
+            component:CreateComponent
+      },
+      {
+            path:'edit',
+            component:EditComponent
+      },
+      {
+            path:'login',
+            component:LoginComponent
+      },
+      {
+            path:'register',
+            component:RegisterComponent
+      }
+];
